0eb6aceaf44312534e9f16fbca8ac11876f08fcf
ibalram <balramsinghrathore3@gmail.com>
2020-09-23 20:24:25 +0530

    added usecase2 - Calculate Daily Wage

---
+++ src/com/capgemini/CalculateDailyWage.java
@@ -0,0 +1,12 @@
+package com.capgemini;
+
+// Usercase2
+public class CalculateDailyWage {
+	public static void main(String[] args) {
+		int wagePerHour = 20;
+		int dayHour = 8;
+		int dailyWage = wagePerHour*dayHour;
+		System.out.println(dailyWage);
+	}
+
+}
