f747b9315c1a17015783e7c0ef5890050e2e0ee9
ibalram <balramsinghrathore3@gmail.com>
2020-09-23 22:14:28 +0530

    reformated

--- src/com/capgemini/CalculateDailyWage.java
+++ src/com/capgemini/CalculateDailyWage.java
@@ -3,10 +3,14 @@ package com.capgemini;
 // Usercase2
 public class CalculateDailyWage {
 	public static void main(String[] args) {
-		int wagePerHour = 20;
-		int dayHour = 8;
-		int dailyWage = wagePerHour*dayHour;
-		System.out.println(dailyWage);
+		int EMP_RATE_PER_HOUR = 20;
+		int IS_FULL_TIME = 1;
+		double check = Math.floor(Math.random()*10)%2;
+		int dayHour = 0;
+		if (check==IS_FULL_TIME)
+			dayHour = 8;
+		int dailyWage = EMP_RATE_PER_HOUR*dayHour;
+		System.out.println("Emp Wage: " + dailyWage);
 	}
 
 }
